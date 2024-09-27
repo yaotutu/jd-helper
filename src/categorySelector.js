4e0b18d2f814b7784f8a5ad00a4a16d6338abc82
yaotutu <957810668@qq.com>
2024-09-27 16:00:01 +0800

    ♻️ 重构(categorySelector.js)：抽取waitForSelector函数以提高可读性   ✨ 功能(categorySelector.js)：在等待类目选择过程中添加错误处理功能

--- src/categorySelector.js
+++ src/categorySelector.js
@@ -2,6 +2,11 @@ import { taskData } from "./taskData.js";
 import { clickButton } from "./puppeteerHelper.js";
 const { stepOne } = taskData;
 
+// 自定义 waitForSelector 函数
+const waitForSelector = async (page, selector, timeout = 10000) => {
+  await page.waitForSelector(selector, { timeout });
+};
+
 // 点击商品类目
 const clickCategoryItem = async (page, containerSelector, targetText) => {
   console.log(`点击包含"${targetText}" 的 .vc-category-column__item`);
@@ -33,29 +38,47 @@ const clickCategoryItem = async (page, containerSelector, targetText) => {
 
 // 等待并点击商品类目
 const waitForAndClickCategory = async (page, category) => {
-  await page.waitForSelector(category.containerSelector, { timeout: 10000 });
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  const clicked = await clickCategoryItem(
-    page,
-    category.containerSelector,
-    category.targetText
-  );
-  if (clicked) {
-    console.log(
-      `已成功点击包含"${category.targetText}" 的 .vc-category-column__item`
+  try {
+    await waitForSelector(page, category.containerSelector);
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 等待一段时间以确保页面稳定
+    const clicked = await clickCategoryItem(
+      page,
+      category.containerSelector,
+      category.targetText
     );
-  } else {
-    console.log(
-      `未找到包含 "${category.targetText}" 的 .vc-category-column__item`
-    );
+    if (clicked) {
+      console.log(
+        `已成功点击包含"${category.targetText}" 的 .vc-category-column__item`
+      );
+    } else {
+      console.log(
+        `未找到包含 "${category.targetText}" 的 .vc-category-column__item`
+      );
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
   }
 };
 
+// 主函数
 export const selectCategoriesAndProceed = async (page) => {
-  await waitForAndClickCategory(page, stepOne.firstCategory);
-  await waitForAndClickCategory(page, stepOne.secondCategory);
-  await waitForAndClickCategory(page, stepOne.thirdCategory);
-  await clickButton(page, stepOne.nextStepButton);
+  const categories = [
+    stepOne.firstCategory,
+    stepOne.secondCategory,
+    stepOne.thirdCategory,
+  ];
+
+  for (const category of categories) {
+    await waitForAndClickCategory(page, category);
+  }
+
+  // 点击下一步按钮
+  try {
+    await clickButton(page, stepOne.nextStepButton);
+    console.log("已成功点击下一步按钮");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 
   // 等待页面跳转完成
   await new Promise((resolve) => setTimeout(resolve, 9000));
